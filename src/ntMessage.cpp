171cc37a871d94cd9e4c207738a9eca1f8e9bd25
Christopher Schmidt <Pfannkuchen2@googlemail.com>
2012-05-11 16:17:51 +0200

    added new ntMessage class

---
+++ src/ntMessage.cpp
@@ -0,0 +1,90 @@
+/*==LICENSE==
+
+This file is part of Lemuria.
+Copyright (C) 2012 Florian Meiﬂner
+
+Lemuria is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lemuria is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lemuria. If not, see <http://www.gnu.org/licenses/>.
+
+*==LICENSE==*/
+
+#include "ntMessage.h"
+
+//ctor---------------------
+//out----------------------
+ntMessage::ntMessage(int clientID)
+: fClientID(clientID) {
+}
+
+//ctor--------------------
+//in----------------------
+ntMessage::ntMessage(Packet *packet)
+: ntPacket(packet) {
+    ntFlag = ntPacket->data[0];
+    streamIn = BitStream(ntPacket->data, ntPacket->length, false);
+    streamIn.IgnoreBytes(sizeof(MessageID));
+
+    streamIn.Read(fClientID);
+}
+
+//dtor-------------------
+ntMessage::~ntMessage() {
+    streamIn.Reset();
+    streamOut.Reset();
+}
+
+//read
+Vector3 ntMessage::readVector() {
+    int x, y, z;
+
+    streamIn.Read(x);
+    streamIn.Read(y);
+    streamIn.Read(z);
+
+    return Vector3(x, y, z);
+}
+
+crPlayer *ntMessage::readPlayer() {
+    int walking, turning, x, y, z;
+    float yaw;
+    crPlayer *player;
+
+    streamIn.Read(walking);
+    streamIn.Read(turning);
+    streamIn.Read(x);
+    streamIn.Read(y);
+    streamIn.Read(z);
+    streamIn.Read(yaw);
+
+    player = new crPlayer(0, fClientID, walking, turning, Vector3(x, y, z), yaw);
+    return player;
+}
+
+//write
+void ntMessage::writeHeader() {
+    streamOut.Write((MessageID)ntFlag);
+    streamOut.Write(fClientID);
+}
+
+void ntMessage::writePlayer(crPlayer *player) {
+    streamOut.Write(player->fIsWalking);
+    streamOut.Write(player->fIsTurning);
+    streamOut.Write(player->fPosition.x);
+    streamOut.Write(player->fPosition.y);
+    streamOut.Write(player->fPosition.z);
+    streamOut.Write(player->fYaw);
+}
+
+void ntMessage::writePlayerName(RakString name) {
+    streamOut.Write(name);
+}
